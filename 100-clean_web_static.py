f8a5a29e2b4b95b457e36f053e7654421c7d02e8
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2023-04-06 00:43:41 +0100

    Update 100-clean_web_static.py

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -8,7 +8,7 @@ fab -f 100-clean_web_static.py do_clean:number=2
 import os
 from fabric.api import *
 
-env.hosts = ['35.243.128.200', '3.239.120.96']
+env.hosts = ['52.87.155.66', '54.89.109.87']
 
 
 def do_clean(number=0):
