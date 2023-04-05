586a86260792940c4de77dbfab60548ac5700a4b
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2023-04-05 23:49:28 +0100

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,44 +1,18 @@
-#!/usr/bin/env bash
-# Prepare my webservers (web-01 & web-02)
+sudo apt-get update
+sudo apt-get -y install nginx
 
-# uncomment for easy debugging
-#set -x
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+sudo touch /data/web_static/releases/test/index.html
+sudo echo "<html><head></head><body>Holberton School</body></html>" | sudo tee /data/web_static/releases/test/index.html
 
-# colors
-blue='\e[1;34m'
-#brown='\e[0;33m'
-green='\e[1;32m'
-reset='\033[0m'
+sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
 
-echo -e "${blue}Updating and doing some minor checks...${reset}\n"
-
-# install nginx if not present
-if [ ! -x /usr/sbin/nginx ]; then
-	sudo apt-get update -y -qq && \
-	     sudo apt-get install -y nginx
-fi
-
-echo -e "\n${blue}Setting up some minor stuff.${reset}\n"
-
-# Create directories...
-sudo mkdir -p /data/web_static/releases/test /data/web_static/shared/
-
-# create index.html for test directory
-echo "<h1>Welcome to th3gr00t.tech <\h1>" | sudo dd status=none of=/data/web_static/releases/test/index.html
-
-# create symbolic link
-sudo ln -sf /data/web_static/releases/test /data/web_static/current
-
-# give user ownership to directory
 sudo chown -R ubuntu:ubuntu /data/
 
-# backup default server config file
-sudo cp /etc/nginx/sites-enabled/default nginx-sites-enabled_default.backup
-
-# Set-up the content of /data/web_static/current/ to redirect
-# to domain.tech/hbnb_static
-sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web/static/current/;}' /etc/nginx/sites-available/default
+sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
 
 sudo service nginx restart
-
-echo -e "${green}Completed${reset}"
