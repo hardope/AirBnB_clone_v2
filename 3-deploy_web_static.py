7d2f0bb8ddf7bb3a9161f292727d19a1d04b26b7
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2023-04-06 11:24:48 +0100

    Update 3-deploy_web_static.py

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -2,6 +2,8 @@
 """
 Fabric script based on the file 2-do_deploy_web_static.py that creates and
 distributes an archive to the web servers
+
+execute: fab -f 3-deploy_web_static.py deploy -i ~/.ssh/id_rsa -u ubuntu
 """
 
 from fabric.api import env, local, put, run
