d95290a0b9a37c82dfc5cd322d52306450bb7716
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2023-04-06 08:38:09 +0000

    Updated

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -21,3 +21,4 @@ sudo chown -R ubuntu:ubuntu /data/
 sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
 
 sudo service nginx restart
+
