96a7ae99e094c816d7d82ea986408677574d68e4
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2023-04-20 07:59:16 +0000

    Updated

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-starts a Flask web application
+start Flask application
 """
 
 from flask import Flask, render_template
--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """
-starts a Flask web application
-"""
+starts a Flask web applicationstart Flask application"""
 
 from flask import Flask, render_template
 from models import *
